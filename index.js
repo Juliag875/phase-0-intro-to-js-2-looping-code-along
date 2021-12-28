47ab9712d7ae7681a19b93c4523f62290b4185b6
Iuliia El Sammak <juliag875@gmail.com>
2021-12-27 20:07:30 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 // Code your solutions in this file
 
-// const names = ["Guadalupe", "Ollie", "Aki"];
-
 function writeCards(names, event) {
     let messages =[];
     for (let i = 0; i < names.length; i++) {
